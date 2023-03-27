b2f71cf860e3b534a0850fe86e6574668ddb5f3d
NadyaDor <nadya.dor@yandex.ru>
2023-03-27 11:21:02 +0500

    third

--- scripts/index.js
+++ scripts/index.js
@@ -5,11 +5,9 @@ const popup = document.querySelector(".popup");
 //  находим находим в попапе редактированиея профайла кнопку крест
 const popupClose = popup.querySelector(".popup__close");
 // добавляем ввод имени
-const popupNameInputName = popup.querySelector(".popup__name-input_name");
+const popupNameInputTypeName = popup.querySelector(".popup__name-input_type_name");
 // добавляем ввод описания под именем
-const popupNameInputAbout = popup.querySelector(".popup__name-input_about");
-// добавляем кнопку сохранить
-const popupButtonEdit = popup.querySelector(".popup__button-edit");
+const popupNameInputTypeAbout = popup.querySelector(".popup__name-input_type_about");
 // добавили поиск формы, в которой в попапе вписываем новые данные
 const popupForm = popup.querySelector(".popup__form");
 // добавила месту, куда в профайле запишется новое имя
@@ -22,16 +20,20 @@ const profileAbout = document.querySelector(".profile__about");
 editButton.addEventListener('click', () => {
   // добавляем модификатор, который сделает попап с редактированием профайла видимым
   popup.classList.add('popup_opened');
+  // присваиваем в строках ввода значения из профайла, если этого не сделать, то строки для ввода останутся пустыми
+  popupNameInputTypeName.value = profileName.innerHTML;
+  popupNameInputTypeAbout.value = profileAbout.innerHTML;
 });
 
 // добавляем слушатель сабмита на кноgке сохранить в форме попапа. В скобки добавляем event
 popupForm.addEventListener('submit', (event) => {
   event.preventDefault(); // обязательно при работе с формами
-  const name = popupNameInputName.value;
-  const about = popupNameInputAbout.value;
+  const name = popupNameInputTypeName.value;
+  const about = popupNameInputTypeAbout.value;
   // указываю куда сохранятся изменения
   profileName.textContent = name;
   profileAbout.textContent = about;
+  popup.classList.remove('popup_opened');
 });
 
 // добавляю слушатель к кнопке крестик
